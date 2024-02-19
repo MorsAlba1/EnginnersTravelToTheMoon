83e255cd481507fdb373a4420181c11e6ec47998
r3ad1ly <151501995+MorsAlba1@users.noreply.github.com>
2024-02-18 22:45:11 -0600

    Actualizar scripts

---
+++ docs/scripts/texto3d.js
@@ -0,0 +1,5 @@
+function startAudio() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('myAudio').style.display = 'block';
+    document.getElementById('myAudio').play();
+}
